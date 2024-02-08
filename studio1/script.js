aeba80b538009a6fb2189f3b22138dbda46950f4
isaaabella1 <imintersimone@ucdavis.edu>
2024-02-08 02:06:30 -0800

    madlibs done

--- studio1/script.js
+++ studio1/script.js
@@ -4,6 +4,7 @@
 
     const myForm = document.getElementById("myform");
     const madlib = document.getElementById("madlib")
+    const cupWritten = document.getElementById("cupWritten")
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
         const place = document.querySelector("#place").value;
@@ -17,7 +18,7 @@
         const money = document.querySelector("#money").value;
         const food = document.querySelector("#food").value;
         let myText;
-
+        let writing;
         if (place == '') {
             myText = "Please provide a place";
             document.querySelector('#place').focus();
@@ -50,6 +51,7 @@
             document.querySelector('#food').focus();
         } else {
             myText = 
+
             `<p>Barista: Hi, welcome to ${place}, what can I get started for you?</p> <p>Customer: Can I get a ${number}-shot ${flavor} latte with ${liquid} milk?</p>
             <p>Barista: Yeah, did you want that ${adj} or iced?</p>
             <p>Customer: I'll do it ${adj}, and can I get a/an ${food} as well, and that will be all!</p>
@@ -57,8 +59,75 @@
             <p>Customer: Yeah, it's for ${name}</p>  
             <p>Barista: Alright, your total is $${money} and that will be out in ${time}.</p>
             <p>Customer: Thanks</p> <p>Barista: Have a nice ${time2}!</p>`;
+            writing = 
+                  
+            `<img src="images/CoffeeCup.png" alt="CoffeeCup">
+            <p id="shotsPos">${number}</p>
+            <p id="MilkPos">${liquid}</p>
+            <p id="SyrupPos">${flavor}</p>
+            <p id="namePos"> ${changeName(document.querySelector('#name').value)}</p>
+            <p id=closeMessage>Enjoy your coffee! Press Esc to order again!</p>`;
+
         }
-        
+       
+        //NEEDS ADDING TO Overlay
         madlib.innerHTML = myText;
+        cupWritten.innerHTML = writing;
+        document.querySelector('#overlay').appendChild(cupWritten);
+        document.querySelector('#overlay').className = "showing";
+        //document.
+
+    });
+    function changeName(userName) {
+        userName = userName.toLowerCase();
+        let modifiedName = '';
+
+        for (var i = 0; i < userName.length; i++) {
+            let char = userName.charAt(i);
+            switch (char) {
+                case 'a':
+                    modifiedName += 'ae';
+                    break;
+                case 'e':
+                    modifiedName += 'i';
+                    break;
+                case 'u':
+                    
+                    modifiedName += 'oo';
+                    break;
+                case 'o':
+                    modifiedName += 'u';
+                    break;
+                case 'y':
+                    modifiedName += 'eigh';
+                    break;
+                case 'f':
+                    modifiedName += 'ph';
+                    break;
+                case 'ph':
+                    modifiedName += 'f';
+                    break;
+                default:
+                    modifiedName += char;
+                    break;
+            }
+        }
+        return modifiedName;
+    };
+    document.addEventListener('keydown', function(event){
+        if (event.key==='Escape'){
+            document.querySelector('#overlay').className = "hidden";
+            myForm.reset(); 
+            madlib.innerHTML = ''; 
+            cupWritten.innerHTML = '';
+        }
+
+    });
+    document.querySelector('.close').addEventListener('click',function(event){
+        event.preventDefault();
+        document.querySelector('#overlay').className = "hidden";
+        myForm.reset(); 
+        madlib.innerHTML = ''; 
+        cupWritten.innerHTML = '';
     });
 })();
