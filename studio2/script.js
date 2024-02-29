efdb8a62f9d6dabd48368bf90410ed95787ee797
isaaabella1 <imintersimone@ucdavis.edu>
2024-02-28 23:53:38 -0800

    Sstudio two added

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,20 @@
-(function(){
+(function() {
     'use strict';
-    console.log('reading js');
+
+    var heading = document.querySelector('h1');
+    var overlays = document.querySelectorAll('.overlay');
+
+    document.querySelectorAll('p').forEach(function(paragraph, index) {
+        var displayText = paragraph.getAttribute('data-text');
+
+        paragraph.addEventListener('mouseover', function() {
+            heading.innerHTML = displayText;
+            overlays[index].style.display = 'block';
+        });
+
+        paragraph.addEventListener('mouseout', function() {
+            heading.innerHTML = "Hover over a box to see a sketch";
+            overlays[index].style.display = 'none';
+        });
+    });
 })();
